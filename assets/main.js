19308287e0bf5d3ee75dd850db5e6d4228f3e17a
Your Namede <edoardo.monti.bo@gmail.com>
2023-04-12 17:08:33 +0200

    gestione degli oggetti nell'array

--- assets/main.js
+++ assets/main.js
@@ -12,3 +12,51 @@ Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede
 */
+
+
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+  ];
+
+  
+
+  for(i = 0; i < team.length; i++){
+    console.log(team[i].name)
+    console.log(team[i].role)
+    console.log(team[i].image)
+
+    document.querySelector(".container").innerHTML += team[i].name
+    document.querySelector(".container").innerHTML += team[i].role
+    document.querySelector(".container").innerHTML += team[i].image
+
+    
+  }
